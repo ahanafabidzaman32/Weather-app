bd251f3d5be4893cedc896f3248f401b845bb17f
ahanafabdzaman32 <ahanafabidzaman@gmail.com>
2022-12-20 23:54:23 +0600

    get data from api

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// let xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 
-// xhr.open("get","https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720");
-// xhr.send();
+xhr.open("get","https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720");
+xhr.send();
 
-// xhr.onload = ()=>{
-//     console.log(JSON.parse(xhr.responseText))
-// };
+xhr.onload = ()=>{
+console.log(JSON.parse(xhr.responseText))
+};
 
 
