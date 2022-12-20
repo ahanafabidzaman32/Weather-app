981e5fd9ccd443569f0f3cdc03909e120960e85d
ahanafabdzaman32 <ahanafabidzaman@gmail.com>
2022-12-20 23:50:55 +0600

    xml data

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var datetime = new Date();
-document.getElementById("time").textContent = datetime.toTimeString().slice(0, 8); //it will print on html page
+// let xhr = new XMLHttpRequest();
+
+// xhr.open("get","https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720");
+// xhr.send();
+
+// xhr.onload = ()=>{
+//     console.log(JSON.parse(xhr.responseText))
+// };
+
+
