5137cdf731fc45e917ccd76277fcf45149521e29
ahanafabdzaman32 <ahanafabidzaman@gmail.com>
2022-12-21 15:05:34 +0600

    added live humidity

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ xhr.open("get","https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10
 xhr.send();
 
 xhr.onload = ()=>{
-console.log(JSON.parse(xhr.responseText))
+const weatherInfo = JSON.parse(xhr.responseText)
+console.log(weatherInfo)
+document.getElementById('humidity').innerText = weatherInfo.main.humidity
 };
 
+
+// const myRequest = new Request('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720');
+// fetch(myRequest).then((response) => {console.log(response)})
 
