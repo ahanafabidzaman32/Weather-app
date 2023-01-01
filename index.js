c4f67e66068bb93023d5abd594d4b76831bba992
ahanafabdzaman32 <ahanafabidzaman@gmail.com>
2023-01-01 23:03:38 +0600

    Integrate Real API data, closes #11, #13

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-let xhr = new XMLHttpRequest();
-
-xhr.open("get","https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720");
-xhr.send();
-
-xhr.onload = ()=>{
-const weatherInfo = JSON.parse(xhr.responseText)
-console.log(weatherInfo)
-document.getElementById('humidity').innerText = weatherInfo.main.humidity
-document.getElementById("wind").innerText = weatherInfo.wind.speed
-document.getElementById("temperature").innerText = weatherInfo.main.temp
-
-document.getElementById("icon").src = 'http://openweathermap.org/img/w/' + weatherInfo.weather[0].icon + '.png'
-
-
-
-};
-
 // let locationIcon = document.querySelector('.weather-icon');
 // const {icon} = data.weather[0];
 // locationIcon.innerHTML = <img src="icons/${icon}.png">;
@@ -25,3 +7,19 @@ document.getElementById("time").textContent = datetime.toTimeString().slice(0, 8
 
 // const myRequest = new Request('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720');
 // fetch(myRequest).then((response) => {console.log(response)})
+
+navigator.geolocation.getCurrentPosition(pos => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("get", `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=14ab3f36ef1f7f65cea0b4e134fad720&units=metric`);
+    xhr.send();
+
+    xhr.onload = () => {
+        const weatherInfo = JSON.parse(xhr.responseText)
+        document.getElementById('humidity').innerText = weatherInfo.main.humidity
+        document.getElementById("wind").innerText = weatherInfo.wind.speed
+        document.getElementById("temperature").innerText = weatherInfo.main.temp
+
+        document.getElementById("icon").src = 'http://openweathermap.org/img/w/' + weatherInfo.weather[0].icon + '.png'
+    };
+})
