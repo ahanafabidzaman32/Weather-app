7b9a5b5e55373f71245c418bc36a3f19bb2c8f48
ahanafabdzaman32 <ahanafabidzaman@gmail.com>
2023-01-02 14:01:29 +0600

    get live map location, closes #14

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ document.getElementById("time").textContent = datetime.toTimeString().slice(0, 8
 
 // const myRequest = new Request('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=14ab3f36ef1f7f65cea0b4e134fad720');
 // fetch(myRequest).then((response) => {console.log(response)})
-
+// "https://www.google.com/maps/embed/v1/view?key=AIzaSyDNMgm8b1mwD4P1iCJiaMdzKFfIXSGG-3g&center=24.3634014,88.6048212&zoom=15"
 navigator.geolocation.getCurrentPosition(pos => {
     let xhr = new XMLHttpRequest();
 
     xhr.open("get", `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=14ab3f36ef1f7f65cea0b4e134fad720&units=metric`);
     xhr.send();
-
+    console.log(pos)
+    console.log(pos.coords.latitude,pos.coords.longitude)
+    document.getElementById('map').src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyDNMgm8b1mwD4P1iCJiaMdzKFfIXSGG-3g&center=${pos.coords.latitude},${pos.coords.longitude}&zoom=15`
     xhr.onload = () => {
         const weatherInfo = JSON.parse(xhr.responseText)
         document.getElementById('humidity').innerText = weatherInfo.main.humidity
